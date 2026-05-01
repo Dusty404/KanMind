2840b53ac315cbc68d728b5d1113cea7d923c012
Dusty404 <165195494+Dusty404@users.noreply.github.com>
2026-05-01 20:09:11 +0200

    create project // create auth_app with api // create registration // complete regestration

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -2,15 +2,29 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegistrationSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken.models import Token
 
 
 class RegistrationView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
-
         if serializer.is_valid():
-            user = serializer.save()
+            saved_account = serializer.save()
+            token, created = Token.objects.get_or_create(user=saved_account)
+            return Response({
+                "token": token.key,
+                "fullname": saved_account.profile.fullname,
+                "email": saved_account.email,
+                "user_id": saved_account.id
 
-            return Response({"user dtata": user.id + user.email, "fullname": user.profile.fullname}, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            }, status=status.HTTP_201_CREATED)        
+        return Response({"Ungültige Anfragedaten"}, status=status.HTTP_400_BAD_REQUEST)
+    
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response({"detail": "Logout successful. Token was deleted"}, status=status.HTTP_200_OK)
