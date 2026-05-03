40175c3142eb4be6a52338da6943d3a4f134847b
Dusty404 <165195494+Dusty404@users.noreply.github.com>
2026-05-03 16:36:34 +0200

    create login

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import RegistrationView, CustomLoginView, BoardListView, BoardDetailView
+from rest_framework import routers
+
+#router = routers.SimpleRouter()
+#router.register(r'boards', BoardsViewSet)
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from .serializers import RegistrationSerializer
+from rest_framework import status, generics
+from ..models import Board
+from .serializers import RegistrationSerializer, CustomLoginSerializer, BoardsSerializer
+from .permission import IsOwnerOrReadOnly, CustomIsAuthenticated
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import NotAuthenticated
 
 
 class RegistrationView(APIView):
@@ -22,9 +26,19 @@ class RegistrationView(APIView):
             }, status=status.HTTP_201_CREATED)        
         return Response({"Ungültige Anfragedaten"}, status=status.HTTP_400_BAD_REQUEST)
     
-class LogoutView(APIView):
-    permission_classes = [IsAuthenticated]
 
+class CustomLoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+    serializer_class = CustomLoginSerializer
     def post(self, request):
-        request.user.auth_token.delete()
-        return Response({"detail": "Logout successful. Token was deleted"}, status=status.HTTP_200_OK)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key,
+                "fullname": user.profile.fullname,
+                "email": user.email,
+                "user_id": user.id
+            }, status=status.HTTP_200_OK)
+        return Response({"Ungültige Anfragedaten"}, status=status.HTTP_400_BAD_REQUEST)
